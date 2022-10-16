1c3171d7cd748aa3c4aa4aa14a0445487fedff98
weslleylks <83431453+weslleylks@users.noreply.github.com>
2022-10-16 15:21:29 -0300

    Update LEDS.sh

--- lab02/LEDS.sh
+++ lab02/LEDS.sh
@@ -18,59 +18,61 @@ fi
 
 echo "O comando passado foi: $1"
 
-if ["$1" == "ligar"]; then
-	#Habilitando o LED Vermelho
-	echo "Habilitando a GPIO número $LED_GPIO_VERM"
-	echo LED_GPIO_VERM >> "/sys/class/gpio/export"
-	sleep 1
-	echo "out" >> "/sys/class/gpio/gpio$LED_GPIO_VERM/direction"
+for i in 1 2 3 4 5; do
+	if ["$1" == "ligar"]; then
+		#Habilitando o LED Vermelho
+		echo "Habilitando a GPIO número $LED_GPIO_VERM"
+		echo LED_GPIO_VERM >> "/sys/class/gpio/export"
+		sleep 1
+		echo "out" >> "/sys/class/gpio/gpio$LED_GPIO_VERM/direction"
 	
-	echo "Acendendo o LED"
-	echo 1 >> "/sys/class/gpio/gpio$LED_GPIO_VERM/value"
+		echo "Acendendo o LED"
+		echo 1 >> "/sys/class/gpio/gpio$LED_GPIO_VERM/value"
 	
-	sleep 2
+		sleep 2
 	
-	echo "Desligando o LED"
-	echo 0 >> "/sys/class/gpio/gpio$LED_GPIO_VERM/value"
+		echo "Desligando o LED"
+		echo 0 >> "/sys/class/gpio/gpio$LED_GPIO_VERM/value"
 	
-	echo "Desabilitando a GPIO número $LED_GPIO_VERM"
-	echo $LED_GPIO_VERM	>> "/sys/class/gpio/unexport"	
+		echo "Desabilitando a GPIO número $LED_GPIO_VERM"
+		echo $LED_GPIO_VERM	>> "/sys/class/gpio/unexport"	
 
-	#Habilitando o LED Verde
-	echo "Habilitando a GPIO número $LED_GPIO_VERD"
-	echo LED_GPIO_VERD >> "/sys/class/gpio/export"
-	sleep 1
-	echo "out" >> "/sys/class/gpio/gpio$LED_GPIO_VERD/direction"
-	
-	echo "Acendendo o LED"
-	echo 1 >> "/sys/class/gpio/gpio$LED_GPIO_VERD/value"
-	
-	sleep 1
-	
-	echo "Desligando o LED"
-	echo 0 >> "/sys/class/gpio/gpio$LED_GPIO_VERD/value"
-	
-	echo "Desabilitando a GPIO número $LED_GPIO_VERD"
-	echo $LED_GPIO_VERD >> "/sys/class/gpio/unexport"
-	
-	#Habilitando o LED Amarelo
-	echo "Habilitando a GPIO número $LED_GPIO_AM"
-	echo LED_GPIO_AM >> "/sys/class/gpio/export"
-	sleep 1
-	echo "out" >> "/sys/class/gpio/gpio$LED_GPIO_AM/direction"
-	
-	echo "Acendendo o LED"
-	echo 1 >> "/sys/class/gpio/gpio$LED_GPIO_AM/value"
-	
-	sleep 1
-	
-	echo "Desligando o LED"
-	echo 0 >> "/sys/class/gpio/gpio$LED_GPIO_AM/value"
-	
-	echo "Desabilitando a GPIO número $LED_GPIO_AM"
-	echo $LED_GPIO_AM >> "/sys/class/gpio/unexport"
-	
-else
-	echo "Comando não reconhecido."
-	exit 3
-fi
+		#Habilitando o LED Verde
+		echo "Habilitando a GPIO número $LED_GPIO_VERD"
+		echo LED_GPIO_VERD >> "/sys/class/gpio/export"
+		sleep 1
+		echo "out" >> "/sys/class/gpio/gpio$LED_GPIO_VERD/direction"
+
+		echo "Acendendo o LED"
+		echo 1 >> "/sys/class/gpio/gpio$LED_GPIO_VERD/value"
+
+		sleep 1
+
+		echo "Desligando o LED"
+		echo 0 >> "/sys/class/gpio/gpio$LED_GPIO_VERD/value"
+
+		echo "Desabilitando a GPIO número $LED_GPIO_VERD"
+		echo $LED_GPIO_VERD >> "/sys/class/gpio/unexport"
+
+		#Habilitando o LED Amarelo
+		echo "Habilitando a GPIO número $LED_GPIO_AM"
+		echo LED_GPIO_AM >> "/sys/class/gpio/export"
+		sleep 1
+		echo "out" >> "/sys/class/gpio/gpio$LED_GPIO_AM/direction"
+
+		echo "Acendendo o LED"
+		echo 1 >> "/sys/class/gpio/gpio$LED_GPIO_AM/value"
+
+		sleep 1
+
+		echo "Desligando o LED"
+		echo 0 >> "/sys/class/gpio/gpio$LED_GPIO_AM/value"
+
+		echo "Desabilitando a GPIO número $LED_GPIO_AM"
+		echo $LED_GPIO_AM >> "/sys/class/gpio/unexport"
+
+	else
+		echo "Comando não reconhecido."
+		exit 3
+	fi
+done
